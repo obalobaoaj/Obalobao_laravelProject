e47a0c764297dae28fbf841e8aafc158723aa3e9
obalobaoaj <ajobalobao09@gmail.com>
2025-10-10 22:11:40 +0800

    qweqrqw

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::latest()->get();
+        return view('dashboard', compact('students'));
+    }
+
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:students,email',
+            'phone' => 'required|string|max:11',
+            'address' => 'required|string|max:255',
+        ]);
+
+        Student::create($validated);
+
+        return redirect()->route('dashboard')->with('success', 'Student created successfully');
+    }
+}
